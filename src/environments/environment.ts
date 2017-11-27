a5d29239e5a2c806b9980939bd8cd014b7e4a74f
Divaldo Neto <dneto.mathias@gmail.com>
2017-11-27 15:46:02 +0000

    inserido campo admin na query da tela inicial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  backendServerPath: 'https://unisystem-nathaliars.c9users.io/',
+  backendServerPath: 'http://uni-back2-slayper.c9users.io/',
 };
