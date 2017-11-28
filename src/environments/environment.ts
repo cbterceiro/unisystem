71d27482c114fad3df8ff7ec56360895d437d27c
André Luiz <sg.andreluiz@gmail.com>
2017-11-27 23:22:00 -0200

    Alterações de fonte (cor, tamanho, espaçamento)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  backendServerPath: 'http://uni-back2-slayper.c9users.io/',
+  backendServerPath: 'https://unisystem-backend-supramati.c9users.io/',
 };
